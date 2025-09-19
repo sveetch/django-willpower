c5f7cb08dea9d15481035cdeefb6d9646dfbc4c8
David THENON <sveetch@gmail.com>
2025-09-19 01:04:46 +0000

    [CHG] Leveled up pagination settings to 75 entries

--- django_willpower/data/default_stack/django_configuration.py
+++ django_willpower/data/default_stack/django_configuration.py
@@ -6,5 +6,5 @@ class {{ app.get_appcode_pascal_case() }}Settings:
     """
 {% for model_inventory in inventories %}
     # Pagination limit for model '{{ model_inventory.name }}'
-    {{ model_inventory.name|upper }}_LIST_PAGINATION = 50
+    {{ model_inventory.name|upper }}_LIST_PAGINATION = 75
 {% endfor %}
--- django_willpower/data/default_stack/settings.py
+++ django_willpower/data/default_stack/settings.py
@@ -1,4 +1,4 @@
 {% for model_inventory in inventories %}
 # Pagination limit for model '{{ model_inventory.name }}'
-{{ model_inventory.name|upper }}_LIST_PAGINATION = 20
+{{ model_inventory.name|upper }}_LIST_PAGINATION = 75
 {% endfor %}
