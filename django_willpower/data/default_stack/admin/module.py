7093f149fe6953a8409993dc1dd2f72bb35829c8
David THENON <sveetch@gmail.com>
2025-09-19 01:03:49 +0000

    [FIX] Fixed admin module template

--- django_willpower/data/default_stack/admin/module.py
+++ django_willpower/data/default_stack/admin/module.py
@@ -13,8 +13,8 @@ class {{ model_inventory.admin_name }}(admin.ModelAdmin):
     list_display = {{ model_inventory.admin_list_display|tojson }}{% endif %}{% if model_inventory.readonly_fields %}
     readonly_fields = {{ model_inventory.readonly_fields|tojson }}{% endif %}{% if model_inventory.default_order %}
     ordering = {{ model_inventory.default_order|tojson }}{% endif %}{% if model_inventory.list_filter %}
-    list_filter = {{ model_inventory.list_filter|tojson }}{% endif %}{% endif %}{% if model_inventory.search_fields %}
-    search_fields = {{ model_inventory.search_fields|tojson }}{% endif %}{% endif %}{% if model_inventory.admin_inline_models %}
+    list_filter = {{ model_inventory.list_filter|tojson }}{% endif %}{% if model_inventory.search_fields %}
+    search_fields = {{ model_inventory.search_fields|tojson }}{% endif %}{% if model_inventory.admin_inline_models %}
     inlines = {{ model_inventory.admin_inline_models|tojson }}{% endif %}{% if model_inventory.prepopulated_fields %}
     prepopulated_fields = {{ model_inventory.prepopulated_fields|tojson }}{% endif %}{% if model_inventory.autocompleted_fields %}
     autocomplete_fields = {{ model_inventory.autocompleted_fields|tojson }}{% endif %}
